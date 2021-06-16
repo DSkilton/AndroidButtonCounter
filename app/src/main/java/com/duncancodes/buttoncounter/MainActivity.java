24219e611c2054d21bf0a2326b4700fdd0e53eed
Duncan Skilton <MC03353@mbro.ac.uk>
2021-06-16 11:23:35 +0100

    start adding variables to MainActivity

--- app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
+++ app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
@@ -2,13 +2,19 @@ package com.duncancodes.buttoncounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private Button button;
+    private TextView textView; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.note_layout);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.note_layout);
     }
 }
