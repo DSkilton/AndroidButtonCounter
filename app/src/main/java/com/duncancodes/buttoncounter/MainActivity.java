fa8bc16dcb4cb8b07bedc03c2265f36f99e0e558
Duncan Skilton <MC03353@mbro.ac.uk>
2021-06-16 11:34:24 +0100

    added notes and update fields

--- app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
+++ app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
@@ -7,14 +7,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editText;
-    private Button button;
-    private TextView textView; 
+    private EditText userInput; //storing EditText widget data in variable userInput
+    private Button button; //as above, button for button
+    private TextView textView; //as above etc
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        setContentView(R.layout.note_layout);
+
+                    //(EditText) sets the data type
+        userInput =(EditText) findViewById(R.id.editText);  //findViewById returns view object and appropriate
+        button = (Button) findViewById(R.id.button);        //type is assigned using casting
+        textView = (TextView) findViewById(R.id.textView);  //R.id is a class which allows us to search and pass XML @+ID's
+                                                            //Basically, it refers to the widget in the layout
+        
     }
 }
