aac7b64f921c71c30be123969c321a93a29d518d
Duncan Skilton <MC03353@mbro.ac.uk>
2021-06-16 12:54:01 +0100

    added event listener to button. Tested and working

--- app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
+++ app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.duncancodes.buttoncounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInput; //storing EditText widget data in variable userInput
     private Button button; //as above, button for button
     private TextView textView; //as above etc
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);        //type is assigned using casting
         textView = (TextView) findViewById(R.id.textView);  //R.id is a class which allows us to search and pass XML @+ID's
                                                             //Basically, it refers to the widget in the layout
-        
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                numTimesClicked++;
+                String result = "\nThe button has been clicked " + numTimesClicked + " time(s)";
+                textView.append(result);
+            }
+        };
+
+        button.setOnClickListener(ourOnClickListener);
     }
+
+
 }
