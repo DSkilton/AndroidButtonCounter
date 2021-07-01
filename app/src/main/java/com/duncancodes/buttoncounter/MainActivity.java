f3216bcea6317724237fae13bec40312a074a0ad
Duncan Skilton <MC03353@mbro.ac.uk>
2021-07-01 11:07:59 +0100

    cleared editText on load and button click

--- app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
+++ app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText userInput; //storing EditText widget data in variable userInput
-    private Button button; //as above, button for button
     private TextView textView; //as above etc
 
     @Override
@@ -20,17 +19,19 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.note_layout);
 
                     //(EditText) sets the data type
-        userInput =(EditText) findViewById(R.id.editText);  //findViewById returns view object and appropriate
+        userInput = (EditText) findViewById(R.id.editText);  //findViewById returns view object and appropriate
         Button button = (Button) findViewById(R.id.button); //type is assigned using casting
         textView = (TextView) findViewById(R.id.textView);  //R.id is a class which allows us to search and pass XML @+ID's
                                                             //Basically, it refers to the widget in the layout
 
+        userInput.getText().clear();//another way to clear text
         textView.setText("");//clears text view
         textView.setMovementMethod((new ScrollingMovementMethod()));//makes the text view scroll
 
         View.OnClickListener ourOnClickListener = new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                userInput.getText().clear();// clears text on button click
                 String result = userInput.getText().toString();
 //                another way would be to use:
 //                Editable e = userInput.getText();
