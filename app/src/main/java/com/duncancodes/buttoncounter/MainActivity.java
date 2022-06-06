fcf1279d79675e1b5922fa49412fe61ba6a3c3e9
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-06-06 20:49:28 +0100

    added dashboard context and working with mipmap images

--- app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
+++ app/src/main/java/com/duncancodes/buttoncounter/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText userInput; //storing EditText widget data in variable userInput
     private TextView textView; //as above etc
-    private static final String TAG = "MainActivity";
+    private static final String TAG = MainActivity.class.getName();
     private final String TEXT_CONTENTS = "";
 
     @Override
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.note_layout);
 
                     //(EditText) sets the data type
-        userInput = (EditText) findViewById(R.id.editText);  //findViewById returns view object and appropriate
+        userInput = (EditText) findViewById(R.id.editText); //findViewById returns view object and appropriate
         Button button = (Button) findViewById(R.id.button); //type is assigned using casting
         textView = (TextView) findViewById(R.id.textView);  //R.id is a class which allows us to search and pass XML @+ID's
                                                             //Basically, it refers to the widget in the layout
